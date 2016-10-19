1113c2cffe572fff9686bcb8d4d43814e3c4b081
Juliette3529 <Juliette@saint-jalme.fr>
2016-10-19 10:39:38 +0200

    route.php

---
+++ app/route.php
@@ -0,0 +1,59 @@
+<?php
+
+	$app->get(
+		'/read/group/{group_id}/depenses',
+		'Compta\Controller\APIControllerRead::getDepenses'
+	)->bind('read_depenses');
+
+	$app->get(
+		'/read/group/{group_id}/users',
+		'Compta\Controller\APIControllerRead::getUsers'
+	)->bind('read_users');
+
+    $app->get(
+		'/read/group/{group_id}/group',
+		'Compta\Controller\APIControllerRead::getGroups'
+	)->bind('read_groups');
+
+	$app->post(
+		'/add/group',
+		'Compta\Controller\APIControllerCreate::addGroup'
+	)->bind('add_group');
+
+	$app->post(
+		'/add/depense',
+		'Compta\Controller\APIControllerCreate::addDepense'
+	)->bind('add_depense');
+
+	$app->post(
+		'/add/user',
+		'Compta\Controller\APIControllerCreate::addUser'
+	)->bind('add_user');
+
+	$app->delete(
+		'/api/group/{id}',
+		'Compta\Controller\APIController::deleteGroup'
+	)->bind('api_group_delete');
+
+	$app->delete(
+		'/api/depense/{id}',
+		'Compta\Controller\APIController::deleteDepense'
+	)->bind('api_depense_delete');
+
+	$app->delete(
+		'/api/user/{id}',
+		'Compta\Controller\APIController::deleteUser'
+	)->bind('api_user_delete');
+
+    $app->login(
+        '/api/group/{id}',
+        'Compta\Controller\APIController::login'
+    )->bind('api_login');
+
+    $app->logout(
+        '/api/group/{id}',
+        'Compta\Controller\APIController::logout'
+    )->bind('api_logout')
+
+
+?>
