3df1a8617de3947b70de9f33f575f0c704293028
Juliette3529 <Juliette@saint-jalme.fr>
2016-10-21 10:19:04 +0200

    read exception

--- src/Controller/ApiControllerRead.php
+++ src/Controller/ApiControllerRead.php
@@ -8,18 +8,39 @@
 	class APIControllerRead {
 
 		public function getUsers($id, Application $app) {
-			$users = $app['UserDAO']->find($id);
+			try{
+                $users = $app['UserDAO']->find($id);
+            }catch(Exception $e){
+                return $app->json(array(
+                    'records' => [],
+                    'status' => 'KO',
+                    'error' => $e->getMessage()
+                ), 200);
+
+            }
+
 			return $app->json(array(
 				'records' => $result,
                 'status' => 'OK'
 			), 200);
-		}
+        }
         public function getMoney($id, Application $app  ){
-            $users = $app['MoneyDAO']->find($id);
+            try{
+                $users = $app['MoneyDAO']->find($id);
+            }catch(Exception $e){
+                return $app->json(array(
+                    'records' => [],
+                    'status' => 'KO',
+                    'error' => $e->getMessage()
+                ), 200);
+
+            }
+
 			return $app->json(array(
 				'records' => $result,
                 'status' => 'OK'
 			), 200);
+        }
         }
         public function getGroups($id, Application $app  ){
             try{
