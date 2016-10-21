039c7c1b629763d0b10c461134b07b073b73f793
Juliette3529 <Juliette@saint-jalme.fr>
2016-10-21 10:03:40 +0200

    controller findMoney /findGroup

--- src/Controller/ApiControllerRead.php
+++ src/Controller/ApiControllerRead.php
@@ -15,10 +15,18 @@
 			), 200);
 		}
         public function getMoney($id, Application $app  ){
-            throw new \Exception("TODO");
+            $users = $app['MoneyDAO']->find($id);
+			return $app->json(array(
+				'records' => $result,
+                'status' => 'OK'
+			), 200);
         }
         public function getGroups($id, Application $app  ){
-            throw new \Exception("TODO");
+            $users = $app['GroupDAO']->find($id);
+			return $app->json(array(
+				'records' => $result,
+                'status' => 'OK'
+			), 200);
         }
 
     }
