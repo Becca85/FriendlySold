e7867c2037a302341ebd2a5c3d48b839e0e1a50d
Juliette3529 <Juliette@saint-jalme.fr>
2016-10-21 16:37:07 +0200

    corection application silex

--- src/Controller/ApiControllerDelete.php
+++ src/Controller/ApiControllerDelete.php
@@ -1,6 +1,6 @@
 <?php
 namespace Friendly\Controller;
-use Silex\Apllication;
+use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use FriendlySold\Domain\User;
 
@@ -13,35 +13,62 @@ class ApiControllerDelete {
      *
      * @return Article details in JSON format
      */
-    public function deleteUser($id, Application $app) {
-        $user = $app['UserDAO']->delete($id);
-        // Create and return a JSON response
-        return $app->json($responseData);
-    }
-}
+    public function deleteUser($id, Application $app){
+			try{
+                $users = $app['UserDAO']->delete($id);
+            }catch(Exception $e){
+                return $app->json(array(
+                    'records' => [],
+                    'status' => 'KO',
+                    'error' => $e->getMessage()
+                ), 200);
+
+            }
+
+			return $app->json(array(
+				'records' => $result,
+                'status' => 'OK'
+			), 200);
+        }
+
 
 	public function deleteGroup($id, Application $app) {
-        $group = $app['GroupDAO']->delete($id);
-        // Create and return a JSON response
-        return $app->json($responseData);
-    }
+            try {
+                $users = $app['GroupDAO']->delete($id);
+            } catch(Exception $e){
+                return $app->json(array(
+                    'records' => [],
+                    'status' => 'KO',
+                    'error' => $e->getMessage()
+                ), 200);
+
+            }
+
+			return $app->json(array(
+				'records' => $result,
+                'status' => 'OK'
+			), 200);
+        }
+
+
+	public function deleteMoney($id, Application $app){
+
+            try{
+                $users = $app['MoneyDAO']->delete($id);
+            }catch(Exception $e){
+                return $app->json(array(
+                    'records' => [],
+                    'status' => 'KO',
+                    'error' => $e->getMessage()
+                ), 200);
+
+            }
+
+			return $app->json(array(
+				'records' => $result,
+                'status' => 'OK'
+			), 200);
+        }
+
 }
 
-	public function deleteMoney($id, Application $app) {
-        $group = $app['MoneyDAO']->delete($id);
-        // Create and return a JSON response
-        return $app->json($responseData);
-    }
-}
-
-
-
-
-
-/*note exemple*/
-
-
-return $app->json(array('records'=> $result),200);
-
-/*convertion au formation json */
-id => $depense->$ getid();
